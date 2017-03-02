f9e56d908da8023768cc72d5f2a44de08c10e56f
dmytro-revak <dmytro27@gmail.com>
2017-03-02 13:33:28 +0000

    add transfer text from showing area to working area function

--- text-editor-task/js/main.js
+++ text-editor-task/js/main.js
@@ -1,3 +1,31 @@
-  $(document).ready(function() {
-    $('select').material_select();
-  });
+(function initTheEditor() {
+
+    // Variable declaration------------------------------------------------------------------
+
+    // Save needed elements to variables
+    var $showTextContainer = document.querySelector('.showing-text-panel'),
+        $userWorkingArea = document.getElementById('text-editing-area'),
+        $editButton = document.querySelector('.edit-button'),
+        $saveButton = document.querySelector('.save-button');
+
+
+    // Function description-------------------------------------------------------------------
+
+    // Initialised select animation for materialize
+    $(document).ready(function() {
+        $('select').material_select();
+    });
+
+    // Takes all text with showing panel and puts it to editing area
+    function transferTextToEditingArea() {
+        $userWorkingArea.value = $showTextContainer.innerHTML;
+    }
+
+    // Application logic-----------------------------------------------------------------------
+
+    $editButton.onclick = function () {
+        // debugger
+        // var a = $userWorkingArea;
+        transferTextToEditingArea();
+    }
+})();
