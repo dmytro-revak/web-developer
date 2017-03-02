f34506dee01ba8f1ceb7c72a18a3948d16881d9e
dmytro-revak <dmytro27@gmail.com>
2017-03-02 13:45:48 +0000

    join the both of transfer function to one and set it to certain button click

--- text-editor-task/js/main.js
+++ text-editor-task/js/main.js
@@ -16,16 +16,24 @@
         $('select').material_select();
     });
 
-    // Takes all text with showing panel and puts it to editing area
-    function transferTextToEditingArea() {
-        $userWorkingArea.value = $showTextContainer.innerHTML;
+    // If user has pressed 'edit' button followind function gets all text with showing panel and puts it to editing area; If user has pressed 'save' button followind function gets text from editiong panel and puts it to showing panel as HTML markup
+    function transferTextBetweenWorkingAndShowingAreas(clickedElement) {
+        if (clickedElement.name === 'edit') {
+            $userWorkingArea.value = $showTextContainer.innerHTML;
+        } else {
+            $showTextContainer.innerHTML = $userWorkingArea.value;
+        }
     }
 
     // Application logic-----------------------------------------------------------------------
 
+    // Transfer text from showing area to use working area after click on edit button
     $editButton.onclick = function () {
-        // debugger
-        // var a = $userWorkingArea;
-        transferTextToEditingArea();
+        transferTextBetweenWorkingAndShowingAreas(this);
+    }
+
+    // transfer text from editing area to working area as HTML markup
+    $saveButton.onclick = function () {
+        transferTextBetweenWorkingAndShowingAreas(this);
     }
 })();
