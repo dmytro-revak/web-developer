d97cc3ca7ef04f995090f16d1131decd90227fd6
dmytro-revak <dmytro27@gmail.com>
2017-03-03 14:01:46 +0200

    add adding table and adding list panel markup

--- text-editor-task/js/main.js
+++ text-editor-task/js/main.js
@@ -1,12 +1,19 @@
 (function initTheEditor() {
 
-    // Variable declaration------------------------------------------------------------------
+    
+    // Variable declaration-------------------------------------------------------------------
 
     // Save needed elements to variables
     var $showTextContainer = document.querySelector('.showing-text-panel'),
         $userWorkingArea = document.getElementById('text-editing-area'),
         $editButton = document.querySelector('.edit-button'),
-        $saveButton = document.querySelector('.save-button');
+        $saveButton = document.querySelector('.save-button'),
+        $fontSizeInputes = document.querySelectorAll('[name="font-size-value"]'),
+        $fontFamilyList = document.querySelector('.font-family-panel__select-list'),
+        $fontColorList = document.querySelector('.font-color-panel__select-list'),
+        $backgroundColorList = document.querySelector('.background-color-panel__select-list'),
+        $boldTextInput = document.querySelector('.bold-text-toogle__input'),
+        $italicTextInput = document.querySelector('.italic-text-toogle__input');
 
 
     // Function description-------------------------------------------------------------------
@@ -24,7 +31,13 @@
             $showTextContainer.innerHTML = $userWorkingArea.value;
         }
     }
-
+    
+    // Change appropriate font property for showing container. Take property value from changint element 
+    function changeShowingTextPropertyValue(elementWithValue, property) {
+        $showTextContainer.style[property] = elementWithValue.value;
+    }
+    
+    
     // Application logic-----------------------------------------------------------------------
 
     // Transfer text from showing area to use working area after click on edit button
@@ -32,8 +45,61 @@
         transferTextBetweenWorkingAndShowingAreas(this);
     }
 
-    // transfer text from editing area to working area as HTML markup
+    // Transfer text from editing area to working area as HTML markup
     $saveButton.onclick = function () {
         transferTextBetweenWorkingAndShowingAreas(this);
     }
+    
+    // Add changing font-size function for the each fontSize input
+    for (var i = 0; i < $fontSizeInputes.length; i++) {
+        $fontSizeInputes[i].onchange = function () {
+            changeShowingTextPropertyValue(this, 'fontSize');
+        }
+    }
+    
+    // Add changing font-family function for the fontFamily select and when user select to one of available item set showing block that font-family
+    $fontFamilyList.onchange = function () {
+        for (var i = 0; i < $fontFamilyList.length; i++) {
+            if ($fontFamilyList[i].selected) {
+                changeShowingTextPropertyValue($fontFamilyList[i], 'fontFamily');
+            }
+        }
+    }
+    
+    // Add changing font-color function for the fontColor select and when user select to one of available item set showing block that font-color
+    $fontColorList.onchange = function () {
+        for (var i = 0; i < $fontColorList.length; i++) {
+            if ($fontColorList[i].selected) {
+                changeShowingTextPropertyValue($fontColorList[i], 'color');
+            }
+        }
+    }
+    
+    // Add changing background function for the backgroundColor select and when user select to one of available item set showing block that background-color
+    $backgroundColorList.onchange = function () {
+        for (var i = 0; i < $backgroundColorList.length; i++) {
+            if ($backgroundColorList[i].selected) {
+                changeShowingTextPropertyValue($backgroundColorList[i], 'background');
+            }
+        }
+    }
+    
+    // Add changing font-weight for showing panel when user switch the bold-text input 
+    $boldTextInput.onchange = function () {
+        if ($boldTextInput.checked) {
+            changeShowingTextPropertyValue(this, 'fontWeight');
+        } else {
+            $showTextContainer.style.fontWeight = 'normal';
+        }
+    }
+    
+    // Add changing font-style for showing panel when user switch the style-text input 
+    $italicTextInput.onchange = function () {
+        if ($italicTextInput.checked) {
+            changeShowingTextPropertyValue(this, 'fontStyle');
+        } else {
+            $showTextContainer.style.fontStyle = 'normal';
+        }
+    }
+    
 })();
