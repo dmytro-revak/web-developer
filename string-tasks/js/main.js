fdd6fe6db88ef35dd8db61f47a2605b31e4f6d47
dmytro-revak <dmytro27@gmail.com>
2017-03-15 19:57:48 +0200

    create string-tasks document withbasic markup

---
+++ string-tasks/js/main.js
@@ -0,0 +1,9 @@
+(function initStringTasks() {
+    var workingTextArea = document.getElementById('working-text-area'),
+        spacesAmountButton = document.getElementById('spaces-amount-button');
+
+        spacesAmountButton.onclick = function() {
+            var userText  = workingTextArea.value;
+            
+        };
+})();
