c7424e0d855c8a7ed0de54931f42abb7d2a47f1b
dmytro-revak <dmytro27@gmail.com>
2017-03-16 00:14:20 +0200

    add spaces counter function

--- string-tasks/js/main.js
+++ string-tasks/js/main.js
@@ -1,9 +1,21 @@
 (function initStringTasks() {
+
+    // Save working area and page buttons to variables
     var workingTextArea = document.getElementById('working-text-area'),
         spacesAmountButton = document.getElementById('spaces-amount-button');
 
+        // Count amount of spaces 
         spacesAmountButton.onclick = function() {
-            var userText  = workingTextArea.value;
-            
+
+            var userText  = workingTextArea.value,
+                spacesConter = 0;
+
+            for (var i = 0; i < userText.length; i++) {
+                if (userText[i] === ' ') {
+                    spacesConter++
+                }
+            }
+
+            alert('User have entered ' + spacesConter + ' spaces');
         };
 })();
