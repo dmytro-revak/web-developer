206adbdbb3bc1640281e8a24ee9cb8c8031dbf7f
dmytro-revak <dmytro27@gmail.com>
2017-03-15 15:26:11 +0200

    add function witch Change width and height of bouncing-block when user change one of the size inputes

--- bouncing-block-task/js/main.js
+++ bouncing-block-task/js/main.js
@@ -3,8 +3,7 @@
     // Save needed elements to the variables
     var bouncingBlock = document.querySelector('.bouncing-block'),
         creatingButton = document.querySelector('.creating-button'),
-        widthInput = document.querySelector('.width-input'),
-        heightInput = document.querySelector('.height-input'),
+        sizeInputes = document.querySelectorAll('.size-input'),
         chooseStyleButtons = document.querySelectorAll('.choose-style-button'),
         coordinatesButton = document.querySelector('.coordinates-button');
 
@@ -12,5 +11,12 @@
     creatingButton.onclick = function() {
         bouncingBlock.style.display = 'block';
     }
-    
+
+    // Change width and height of bouncing-block when user change one of the size inputes  
+    sizeInputes.forEach(function(input) {
+        input.onchange = function() {
+            bouncingBlock.style[this.name] = this.value + 'px';       
+        }
+    });
+
 })();
