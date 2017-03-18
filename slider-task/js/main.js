38f43038fe1e7f4c870ffec4a119a1fc58db68ae
dmytro-revak <dmytro27@gmail.com>
2017-03-18 22:32:27 +0200

    change activeElementIndex when user click the switch image button

--- slider-task/js/main.js
+++ slider-task/js/main.js
@@ -5,25 +5,25 @@
         return document.querySelectorAll(selector);
     }
 
-    // change activeElement index depending on user button click
-    function switchSliderElement(direction, sliderElements) {
+    // Change activeElement index depending on user button click
+    function changeActiveElementIndex(direction, sliderElements) {
         if (direction === 'left') {
-            activeElementIndex = activeElementIndex > 0 ? activeElementIndex-- : sliderElements.length - 1;
+            activeElementIndex = activeElementIndex > 0 ? activeElementIndex - 1 : sliderElements.length - 1;
         } else {
-            activeElementIndex = activeElementIndex === sliderElements.length ? 0 : activeElementIndex++;
+            activeElementIndex = activeElementIndex === sliderElements.length ? 0 : activeElementIndex + 1;
         }
     }
 
     // Save all page elements to appropriate variables
-    var sliderItems = getEl('slider__item'),
-        switchPictureButtons = getEl('slider__switch-button ');
-
+    var sliderItems = getEl('.slider__item'),
+        switchPictureButtons = getEl('.slider__switch-button');
     // Save default active element index
     var activeElementIndex = 0;
 
+    // Change activeElementIndex when user click the switch image button
     switchPictureButtons.forEach(function (button) {
         button.onclick = function () {
-
+            changeActiveElementIndex(this.dataset.direction, sliderItems);
         }
     });
 
