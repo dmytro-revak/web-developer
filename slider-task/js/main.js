7a01d1cae7743500844626699bd9bec30e76d17e
dmytro-revak <dmytro27@gmail.com>
2017-03-19 01:45:21 +0200

    add function which toggle slider state after each user clicking the play button. Also toggle the state variable and change play button inner html

--- slider-task/js/main.js
+++ slider-task/js/main.js
@@ -1,5 +1,7 @@
 (function initSlider() {
 
+    // Function descriptions //////////////////////////////////////////////////////////////////////////////////////////
+
     // Get needed element
     function getEl(selector) {
         return document.querySelectorAll(selector);
@@ -14,6 +16,7 @@
         }
     }
 
+    // Remove active class from all elements and set it for only choosing one
     function setActiveClass(indexOfActiveElement, sliderElements) {
         sliderElements.forEach(function (currentElement) {
             if ( currentElement.className.indexOf('slider__item_active') !== -1) {
@@ -23,9 +26,12 @@
         sliderElements[indexOfActiveElement].className += ' slider__item_active';
     }
 
+    // Application logic ////////////////////////////////////////////////////////////////////////////////////////
+
     // Save all page elements to appropriate variables
     var sliderItems = getEl('.slider__item'),
-        switchPictureButtons = getEl('.slider__switch-button');
+        switchPictureButtons = getEl('.slider__switch-button'),
+        playButton = getEl('.slider__play-button');
 
     // Save default active element index
     var activeElementIndex = 0;
@@ -33,12 +39,29 @@
     // Change activeElementIndex when user click the switch image button
     switchPictureButtons.forEach(function (button) {
         button.onclick = function () {
-            debugger
             changeActiveElementIndex(this.dataset.direction, sliderItems);
             setActiveClass(activeElementIndex, sliderItems);
-        }
+        };
     });
 
+    // Set default state of slider
+    var isPlaying = false;
 
+    // Toggle slider state after each user clicking the play button. Also toggle the state variable and change play button inner html
+    var sliderPlay;
+    playButton[0].onclick = function () {
+        if (!isPlaying) {
+            sliderPlay = setInterval(function () {
+                changeActiveElementIndex('left', sliderItems);
+                setActiveClass(activeElementIndex, sliderItems);
+            }, 3000);
+            this.innerHTML = 'Pause';
+            isPlaying = true;
+        } else {
+            clearInterval(sliderPlay);
+            this.innerHTML = 'Play';
+            isPlaying = false;
+        }
+    };
 
 })();
