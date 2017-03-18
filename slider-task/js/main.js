b6f84fdd9a21c12c276bf8682de7f3fdf131ca87
dmytro-revak <dmytro27@gmail.com>
2017-03-19 01:15:15 +0200

    add change slider image function and fixed switch activeElement index function

--- slider-task/js/main.js
+++ slider-task/js/main.js
@@ -10,20 +10,32 @@
         if (direction === 'left') {
             activeElementIndex = activeElementIndex > 0 ? activeElementIndex - 1 : sliderElements.length - 1;
         } else {
-            activeElementIndex = activeElementIndex === sliderElements.length ? 0 : activeElementIndex + 1;
+            activeElementIndex = activeElementIndex === sliderElements.length -1 ? 0 : activeElementIndex + 1;
         }
+    }
+
+    function setActiveClass(indexOfActiveElement, sliderElements) {
+        sliderElements.forEach(function (currentElement) {
+            if ( currentElement.className.indexOf('slider__item_active') !== -1) {
+                currentElement.className = currentElement.className.replace(' slider__item_active', '');
+            }
+        });
+        sliderElements[indexOfActiveElement].className += ' slider__item_active';
     }
 
     // Save all page elements to appropriate variables
     var sliderItems = getEl('.slider__item'),
         switchPictureButtons = getEl('.slider__switch-button');
+
     // Save default active element index
     var activeElementIndex = 0;
 
     // Change activeElementIndex when user click the switch image button
     switchPictureButtons.forEach(function (button) {
         button.onclick = function () {
+            debugger
             changeActiveElementIndex(this.dataset.direction, sliderItems);
+            setActiveClass(activeElementIndex, sliderItems);
         }
     });
 
