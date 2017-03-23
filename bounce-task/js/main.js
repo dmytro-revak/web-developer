ebab7b7b471db84a9f439237ef411f5223321b20
dmytro-revak <dmytro27@gmail.com>
2017-03-23 13:44:07 +0200

    add first hover animation

--- bounce-task/js/main.js
+++ bounce-task/js/main.js
@@ -7,4 +7,22 @@
         mousePositionTextWrapper.html('X coordinate = ' + e.pageX + '<br/>' + 'Y coordinate = ' + e.pageY);
     });
 
+    bounceCircle.css({
+        position: 'absolute',
+        top: '50px',
+        left: '20px',
+        height: '25px',
+        width: '25px',
+        borderRadius: '50%',
+        border: '2px solid black'
+    });
+
+    bounceCircle.mouseover(function() {
+        bounceCircle.css({
+            animationName: 'first-hover',
+            animationDuration: '2s',
+            animationFillMode: 'forwards'
+        });
+    });
+
 })();
