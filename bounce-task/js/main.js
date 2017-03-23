e00599ef0144e7ad31bc789cff8cb0de8c11230a
dmytro-revak <dmytro27@gmail.com>
2017-03-23 14:31:35 +0200

    finished with bounce task

--- bounce-task/js/main.js
+++ bounce-task/js/main.js
@@ -17,12 +17,45 @@
         border: '2px solid black'
     });
 
+    var animationNumber  = 1;
+
     bounceCircle.mouseover(function() {
-        bounceCircle.css({
-            animationName: 'first-hover',
-            animationDuration: '2s',
-            animationFillMode: 'forwards'
-        });
+        debugger
+
+        if (animationNumber === 1) {
+            bounceCircle.css({
+                animationName: 'first-hover',
+                animationDuration: '2s',
+                animationFillMode: 'forwards'
+            });
+            animationNumber = 2;
+        } else if (animationNumber === 3) {
+            bounceCircle.css({
+                animationName: 'second-hover',
+                animationDuration: '2s',
+                animationFillMode: 'forwards'
+            });
+            animationNumber = 4;
+        }
+    });
+
+    bounceCircle.click(function() {
+        debugger
+        if (animationNumber === 2) {
+            bounceCircle.css({
+                animationName: 'first-click',
+                animationDuration: '2s',
+                animationFillMode: 'forwards'
+            });
+            animationNumber = 3;
+        } else if (animationNumber === 4) {
+            bounceCircle.css({
+                animationName: 'second-click',
+                animationDuration: '2s',
+                animationFillMode: 'forwards'
+            });
+        }
+
     });
 
 })();
