22e4212a64fed0674af298cca93b78c80a4816b1
dmytro-revak <dmytro27@gmail.com>
2017-03-30 15:29:15 +0300

    add function which change the figure position

--- chess-task/js/main.js
+++ chess-task/js/main.js
@@ -1,21 +1,36 @@
 (function initTheChess() {
 
-    var $allBoardFigures = $('.chess-board__figure'),
+    // save all figures and border cells
+    var $allChessdFigures = $('.chess-board__figure'),
         $borderCells = $('.chess-board__cell');
 
-    var availableCoordinates = [
-        ['a', 'b', 'c', 'd', 'e', 'f', 'h', 'i']
-    ];
+    // allow draggable for all figures
+    $allChessdFigures.draggable();
 
-    $allBoardFigures.draggable({
-    });
-
+    // allow droppable for all board cells
     $borderCells.droppable({
-        drop: function(event, ui) {
 
-            console.log(ui);
-            console.log(this);
+        // change the DOM when user moving a certain figure to certain cell
+        drop: function(event, ui) {
+            var currentFigure = ui.draggable.get(0),
+                currentNewParent = this;
+
+            // move figure from one element into other
+            switchFigurePosition(currentFigure , currentNewParent);
         }
     });
+
+
+    function switchFigurePosition(figure, newParent) {
+
+        // change the parent DOM element for the figure
+        newParent.append(figure);
+
+        // Set it to the center of new parent
+        $(figure).css({
+               top: 0,
+               left: 0
+        });
+    }
 
 })();
