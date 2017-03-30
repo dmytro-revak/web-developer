6b28ae0def0693ab9cc89ae8755b7896798df236
dmytro-revak <dmytro27@gmail.com>
2017-03-30 14:52:53 +0300

    make all chess figures draggable

--- chess-task/js/main.js
+++ chess-task/js/main.js
@@ -1,1 +1,21 @@
-$('.chess-board__cell')
+(function initTheChess() {
+
+    var $allBoardFigures = $('.chess-board__figure'),
+        $borderCells = $('.chess-board__cell');
+
+    var availableCoordinates = [
+        ['a', 'b', 'c', 'd', 'e', 'f', 'h', 'i']
+    ];
+
+    $allBoardFigures.draggable({
+    });
+
+    $borderCells.droppable({
+        drop: function(event, ui) {
+
+            console.log(ui);
+            console.log(this);
+        }
+    });
+
+})();
