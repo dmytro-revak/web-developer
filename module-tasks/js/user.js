1795b711b556e96115f0f15e29e9192707d19867
dmytro-revak <dmytro27@gmail.com>
2017-03-31 19:45:06 +0300

    add module-task directory and make the user task

---
+++ module-tasks/js/user.js
@@ -0,0 +1,26 @@
+var userModule = (function (userName, userAge) {
+    var name = userName,
+        age = userAge,
+        sayName = function () {
+            console.log(name);
+        },
+        sayAge = function () {
+            console.log(age);
+        },
+        setName = function (newName) {
+            name = newName;
+        }
+
+        return {
+            name: name,
+            age: age,
+            sayName: sayName,
+            sayAge: sayAge,
+            setName: setName
+        }
+})('dima', 21);
+
+userModule.sayName();
+userModule.sayAge();
+userModule.setName('Petro');
+userModule.sayName();
