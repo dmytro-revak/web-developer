375a5d11323225bcb4b327e7e495c708151e3392
dmytro-revak <dmytro27@gmail.com>
2017-04-02 13:31:04 +0300

    finished with people task

--- module-tasks/js/peole.js
+++ module-tasks/js/peole.js
@@ -1,33 +1,39 @@
 var people = (function () {
 
+    // make variables to save the names and some page elements
     var names = [],
         $nameList,
         $addButton,
         $nameInput;
 
+    // get needed elements and save them to the variables
     var cacheDom = function () {
         $nameList = $('.people-module__name-list').eq(0);
         $addButton = $('.people-module__add-button').eq(0);
         $nameInput= $('.people-module__name-input').eq(0);
     };
 
+    // add event listener to the add button
     var bindEvent = function () {
         $addButton.click(function () {
-            var currentName = $nameInput.val();
-            addPersonName(currentName);
+            var enteredName = $nameInput.val();
+            addPersonName(enteredName);
         });
     };
 
+    // add new name to the names array
     var addPersonName = function (name) {
         names.push(name);
         renderPage();
     };
 
+    // remove certain name from the names array
     var delPersonName = function (nameIndex) {
         names.splice(nameIndex, 1);
         renderPage();
     };
 
+    // update the page when names array has been changed and show actual names for user
     var renderPage = function () {
         $('.people-module__name-item').remove();
         names.forEach(function (element, index) {
@@ -35,10 +41,8 @@ var people = (function () {
                 $nameListItem = $('<li class="people-module__name-item">'),
                 $delItemBtn = $('<span> X</span>');
 
-        debugger
             $delItemBtn.attr('data-name-index', index);
             $delItemBtn.click(function () {
-                debugger
                 delPersonName(this.dataset.nameIndex);
             });
 
@@ -48,6 +52,7 @@ var people = (function () {
         });
     };
 
+    // inti main html control functions
     var initPeople = function () {
         cacheDom();
         bindEvent();
