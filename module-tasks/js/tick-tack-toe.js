60d4eb5300004e322e77774537d7b334228e0821
dmytro-revak <dmytro27@gmail.com>
2017-04-03 00:08:45 +0300

    add markup to tha play game template

--- module-tasks/js/tick-tack-toe.js
+++ module-tasks/js/tick-tack-toe.js
@@ -3,7 +3,8 @@ var tickTackToeModule = (function () {
     // save module and basic templates
     var $applicationContainer = $('.tick-tack-toe-module'),
         $startGameTemplate = $('.start-game-template'),
-        $initializePlayerTemplate = $('.initialize-player-template');
+        $initializePlayerTemplate = $('.initialize-player-template'),
+        $playGameTemplate = $('.play-game-template');
 
     // control the start-game template
     var startGame = function () {
@@ -21,17 +22,37 @@ var tickTackToeModule = (function () {
             $firstPlayerImage = $('.first-player__image'),
             $secondPlayerImage = $('.second-player__image'),
             $firstPlayerName = $('.first-player__name'),
-            $secondPlayerName = $('.second-player__name');
+            $secondPlayerName = $('.second-player__name'),
+            $letsStartButton = $('.lets-game-button');
+
+        var isFirstPlayerInit = false,
+            isSecondPlayerInit = false;
 
         $firstPlayerInput.change(function () {
             $firstPlayerName.text( $(this).val() );
-            $firstPlayerImage.attr('src', 'https://robohash.org/' + $(this).val())
+            $firstPlayerImage.attr('src', 'https://robohash.org/' + $(this).val());
+            isFirstPlayerInit = true;
+            if (isFirstPlayerInit && isSecondPlayerInit) {
+                $letsStartButton.css('display', 'block');
+            }
         });
 
         $secondPlayerInput.change(function () {
             $secondPlayerName.text( $(this).val() );
-            $secondPlayerImage.attr('src', 'https://robohash.org/' + $(this).val())
+            $secondPlayerImage.attr('src', 'https://robohash.org/' + $(this).val());
+            isSecondPlayerInit = true;
+            if (isFirstPlayerInit && isSecondPlayerInit) {
+                $letsStartButton.css('display', 'block');
+            }
         });
+
+        $letsStartButton.click(function () {
+            $initializePlayerTemplate.removeClass('app-template_active');
+            $initializePlayerTemplate.addClass('app-template_active');
+        });
+    };
+
+    var playGame = function () {
     };
 
     // control all application
