ef820a933395d982735a349f9a71a5517d244332
dmytro-revak <dmytro27@gmail.com>
2017-04-02 17:25:25 +0300

    add logic to start template

--- module-tasks/js/tick-tack-toe.js
+++ module-tasks/js/tick-tack-toe.js
@@ -1,23 +1,27 @@
 var tickTackToeModule = (function () {
 
-    var applicationContainer = $('.tick-tack-toe-module');
+    var $applicationContainer = $('.tick-tack-toe-module'),
+        $startGameTemplate = $('.start-game-template'),
+        $initializePlayerTemplate = $('.initialize-player-template');
 
-    var startTemplate = [
-        {
-            tag: 'h1',
-            elementClasses: 'initialize-player-heading'
-            elementText: 'Players please enter your names'
-        },
-        {
-            tag: 'h1',
-            elementClasses: 'payers-sections flex'
-            elementText: ''
-        }
-    ];
 
-    var controller = function () {
+    var startGame = function () {
+        var $startGameButton = $('.start-game-button');
 
+        $startGameButton.click(function () {
+            $startGameTemplate.removeClass('app-template_active');
+            $initializePlayerTemplate.addClass('app-template_active');
+        });
     };
 
+    var controller = function () {
+        startGame();
+    };
+
+    return {
+      initTickTackToe: controller
+    };
 
 })();
+
+tickTackToeModule.initTickTackToe();
