6353a7161aff644d9f36fcc5d883036a478432d9
dmytro-revak <dmytro27@gmail.com>
2017-04-02 17:07:06 +0300

    add start game and initialize player template

---
+++ module-tasks/js/tick-tack-toe.js
@@ -0,0 +1,23 @@
+var tickTackToeModule = (function () {
+
+    var applicationContainer = $('.tick-tack-toe-module');
+
+    var startTemplate = [
+        {
+            tag: 'h1',
+            elementClasses: 'initialize-player-heading'
+            elementText: 'Players please enter your names'
+        },
+        {
+            tag: 'h1',
+            elementClasses: 'payers-sections flex'
+            elementText: ''
+        }
+    ];
+
+    var controller = function () {
+
+    };
+
+
+})();
