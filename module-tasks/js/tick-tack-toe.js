42d56f64d370025a7ba93c5deb4003fd6b12ed0e
dmytro-revak <dmytro27@gmail.com>
2017-04-03 03:07:31 +0300

    add all possible win combinations to array and written function which allow add X and 0 to the table

--- module-tasks/js/tick-tack-toe.js
+++ module-tasks/js/tick-tack-toe.js
@@ -53,12 +53,57 @@ var tickTackToeModule = (function () {
     };
 
     var playGame = function () {
+        var $firstPlayerImage = $('.first-player__image-wrapper'),
+            $secondPlayerImage = $('.second-player__image-wrapper'),
+            $playAreaCells = $('.play-area__cell'),
+            activePlayer = 'first',
+            possibleWinCombinations = ['048', '246'];
+
+        // add remaining win combinations to array
+        for (var i = 0; i < 3; i++) {
+            var availableRowCombination = '' + i * 3 + (i * 3 + 1) + (i * 3 + 2),
+                availableColumnCombination = '' + i + (i + 3) + (i + 6);
+            possibleWinCombinations.push(availableColumnCombination, availableRowCombination);
+        }
+
+        var gameXCombination = '',
+            gameYCombination = '';
+
+        $playAreaCells.one('click', function () {
+        debugger
+            if (activePlayer === 'first') {
+                $(this).html('&#215;');
+                activePlayer = 'second';
+
+                gameXCombination += this.dataset.cellCoordinate;
+                var a = possibleWinCombinations.indexOf(gameXCombination);
+                verifyTheWinCombination(gameXCombination)
+            } else {
+                $(this).html('&#9898;');
+                activePlayer = 'first';
+
+                rowCoordinate = this.dataset.row;
+                columnCoordinate = this.dataset.cell;
+
+                gameYCombination += this.dataset.cellCoordinate;
+
+
+                verifyTheWinCombination(gameYCombination)
+            }
+        });
+    };
+
+    var verifyTheWinCombination = function (combination) {
+        if (possibleWinCombinations.indexOf(combination) !== -1) {
+            alert('win');
+        }
     };
 
     // control all application
     var controller = function () {
         startGame();
         initializeThePlayer();
+        playGame();
     };
 
     return {
