8bc6db4fd1b7a230eb9b64998669f20a024cf851
dmytro-revak <dmytro27@gmail.com>
2017-04-10 18:36:10 +0300

    add tasks heading markup and initial list of tasks to controller

---
+++ angular-tasks-list/app/controllers/tasksListCtrl.js
@@ -0,0 +1,11 @@
+tasksListApp.controller('tasksListCtrl', function ($scope) {
+   $scope.tasksList = [
+       'Винести сміття',
+       'Поприбирати',
+       'Написати домашнє',
+       'Підлити квіти',
+       'Вивчити Angular',
+       'Не забути про пари',
+       'Знайти сенс життя'
+   ];
+});
