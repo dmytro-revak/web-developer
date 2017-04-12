2cee84afb40e58cf9a8296d9ed36f70b67ef0895
dmytro-revak <dmytro27@gmail.com>
2017-04-12 16:50:04 +0300

    add function which add new blog post when user click add button

--- angular-blog/app/controllers/blog.controller.js
+++ angular-blog/app/controllers/blog.controller.js
@@ -8,13 +8,36 @@ blogApp.controller('blogCtrl', function ($http) {
         vm.postList = response.data;
     });
 
+    // add new blog post when user click add button
+    vm.addNewPost = function () {
+        if (vm.newPostTopic && vm.newPostMessage) {
+            var newPost = {};
+            newPost.postHeading = vm.newPostTopic;
+            newPost.author = vm.userStatus;
+            newPost.date = vm.getCurrentDate();
+            newPost.message = vm.newPostMessage;
+            vm.postList.push(newPost);
+        }
+    };
 
-    // debugger
+    // function which generated current date for the new post on blog
+    vm.getCurrentDate = function () {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1;
+        var yyyy = today.getFullYear();
 
-    // setTimeout(function () {
-    //     console.log(vm.postList);
-    //
-    // }, 2000);
+        if(dd < 10) {
+            dd = '0' + dd;
+        }
+
+        if(mm < 10) {
+            mm = '0' + mm;
+        }
+
+        return mm + '/' + dd + '/' +yyyy;
+    };
+
 
 
     vm.userStatus = 'Admin';
