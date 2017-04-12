cfe73f7b007865fa13e86b0b4fcc5dd025823f4b
dmytro-revak <dmytro27@gmail.com>
2017-04-12 16:19:46 +0300

    add function which get initial post with  ajax request

--- angular-blog/app/controllers/blog.controller.js
+++ angular-blog/app/controllers/blog.controller.js
@@ -1,5 +1,20 @@
-blogApp.controller('blogCtrl', function () {
+blogApp.controller('blogCtrl', function ($http) {
+
+    // save the $scope as vm
     var vm = this;
+
+    // get initial posts list from posts.json
+    $http.get("./posts.json").then(function(response) {
+        vm.postList = response.data;
+    });
+
+    // debugger
+
+    // setTimeout(function () {
+    //     console.log(vm.postList);
+    //
+    // }, 2000);
+
 
     vm.userStatus = 'Admin';
 
